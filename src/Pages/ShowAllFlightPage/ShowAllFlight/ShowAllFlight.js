bbe911e84a17c268ba7d0c3f051c547fe7456844
mofizulislam0810 <mofizul.islam0810@gmail.com>
2022-08-14 10:26:35 +0600

    14-08-2022

--- src/Pages/ShowAllFlightPage/ShowAllFlight/ShowAllFlight.js
+++ src/Pages/ShowAllFlightPage/ShowAllFlight/ShowAllFlight.js
@@ -8,6 +8,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import NoDataFoundPage from "../../NoDataFoundPage/NoDataFoundPage/NoDataFoundPage";
 import useAuth from "../../../hooks/useAuth";
 import Loading from "../../Loading/Loading";
+import { RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack } from "@chakra-ui/react";
 
 const ShowAllFlight = ({
   fetchFlighData,
@@ -66,6 +67,9 @@ const ShowAllFlight = ({
   };
 
   let dataPrice = [];
+
+
+
 
   if (parseInt(radioname) === 0 && name.length === 0) {
     dataPrice = jsonData?.filter(
@@ -272,7 +276,7 @@ const ShowAllFlight = ({
             <div className="container">
               <div className="row px-2">
                 <div className="col-lg-6 mt-3">
-                  <h6 className="float-start text-color fw-bold">Price</h6>
+                  <h6 className="float-start text-color fw-bold">Price {price}</h6>
                 </div>
                 <div className="col-lg-6 mt-3">
                   <div className="text-end">
@@ -294,6 +298,7 @@ const ShowAllFlight = ({
                       name="flexRadioDefault2"
                       value={price}
                       id="flexRadioDefault1"
+                      step="0.01"
                       onInput={handleInput}
                       min={mainJson?.minMaxPrice?.minPrice}
                       max={mainJson?.minMaxPrice?.maxPrice}
@@ -307,6 +312,22 @@ const ShowAllFlight = ({
                       MAX {currency!==undefined ? currency : "BDT"}   {mainJson?.minMaxPrice?.maxPrice}
                     </span>
                   </div>
+{/* <RangeSlider defaultValue={[mainJson?.minMaxPrice?.minPrice, mainJson?.minMaxPrice?.maxPrice]} min={mainJson?.minMaxPrice?.minPrice} max={mainJson?.minMaxPrice?.maxPrice} step={0.01}>
+  <RangeSliderTrack bg='red.100'>
+    <RangeSliderFilledTrack bg='tomato' />
+  </RangeSliderTrack>
+  <RangeSliderThumb boxSize={6} index={0} />
+  <RangeSliderThumb boxSize={6} index={1} />
+</RangeSlider> */}
+
+
+                  {/* <RangeSlider aria-label={['min', 'max']} defaultValue={[mainJson?.minMaxPrice?.minPrice, mainJson?.minMaxPrice?.maxPrice]}>
+                    <RangeSliderTrack>
+                      <RangeSliderFilledTrack />
+                    </RangeSliderTrack>
+                    <RangeSliderThumb index={0} />
+                    <RangeSliderThumb index={1} />
+                  </RangeSlider> */}
                 </div>
               </div>
             </div>

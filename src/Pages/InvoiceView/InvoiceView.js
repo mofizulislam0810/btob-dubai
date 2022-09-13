d77f0cee2f9c91019c3731c32cc39483b3450046
mofizulislam0810 <mofizul.islam0810@gmail.com>
2022-09-13 18:39:24 +0600

    last

---
+++ src/Pages/InvoiceView/InvoiceView.js
@@ -0,0 +1,228 @@
+import axios from 'axios';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import Footer from '../SharePages/Footer/Footer';
+import Navbar from '../SharePages/Navbar/Navbar';
+import SideNavBar from '../SharePages/SideNavBar/SideNavBar';
+import { environment } from '../SharePages/Utility/environment';
+import tllLogo from "../../../src/images/logo/logo-combined.png";
+import { useLocation } from 'react-router-dom';
+
+const InvoiceView = () => {
+    let [invoice, setInvoice] = useState([]);
+    const location = useLocation();
+    const handleGetInvoiceList = () => {
+        const getInvoiceList = async () => {
+            let sendObj = { uniqueTransID: location.search.split("=")[1] };
+            const response = await axios.get(
+                environment.getInvoice + '/' + sendObj,
+                environment.headerToken
+            );
+            // console.log(response);
+            setInvoice(response.data);
+        };
+        getInvoiceList();
+    };
+    let [agentInfo, setAgentInfo] = useState([]);
+    const getAgentInfo = async () => {
+        const response = await axios.get(
+            environment.agentInfo,
+            environment.headerToken
+        );
+        console.log(response);
+        setAgentInfo(response.data);
+    };
+    useEffect(() => {
+        getAgentInfo();
+        handleGetInvoiceList();
+    }, []);
+
+    const print = () => {
+        window.print();
+    };
+    console.log(invoice);
+    return (
+        <div>
+            <Navbar></Navbar>
+            <SideNavBar></SideNavBar>
+            <div className="content-wrapper search-panel-bg">
+                <section className="content-header"></section>
+                <ToastContainer position="bottom-right" autoClose={1500} />
+                <section className="content">
+                    <div className="container mt-3 pb-5 py-2">
+                        <div id="ui-view" data-select2-id="ui-view">
+                            <div>
+                                <div className="card box-shadow">
+                                    <div className="card-header">
+                                        {/* <span className="ms-3">
+                                            PNR :&nbsp;
+                                            {ticketingList.length > 0 ? ticketingList[0]?.pnr : ""}
+                                            <strong> </strong>
+                                        </span> */}
+
+                                        <ul id="menu-standard">
+                                            <li id="menu-item">
+                                                <a
+                                                    href="javascript:void(0)"
+                                                    className="btn btn-sm btn-secondary float-right mr-1 d-print-none"
+                                                    onClick={print}
+                                                >
+                                                    <span className="me-1">
+                                                        <i className="fa fa-print"></i>
+                                                    </span>
+                                                    Print
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="card-body">
+                                        <div className="row text-center">
+                                            <h4>INVOICE</h4>
+                                        </div>
+
+                                        <div className="text-start my-2">
+                                            <>
+                                                <img
+                                                    alt="img01"
+                                                    className="p-2"
+                                                    src={tllLogo}
+                                                    style={{ width: "150px", height: "50px" }}
+                                                ></img>
+                                            </>
+                                        </div>
+
+                                        <table class="table table-borderless table-sm">
+                                            <tbody>
+                                                <tr>
+                                                    <td className="text-start bg-white">
+                                                        <address>
+                                                            <span className="fw-bold fs-6">
+                                                                {agentInfo.name}
+                                                            </span>
+                                                            <br />
+                                                            <div
+                                                                className="mt-2"
+                                                                style={{ fontSize: "10px", lineHeight: "12px" }}
+                                                            >
+                                                                {agentInfo.address}
+                                                                <br />
+                                                                Phone: {agentInfo.mobileNo}<br></br>
+                                                                Email: {agentInfo.email}
+                                                            </div>
+                                                        </address>
+
+                                                    </td>
+                                                    <td className="text-end bg-white">
+                                                        <address>
+                                                            <span className="fw-bold fs-6">
+                                                                Triplover Travel Agency LLC
+                                                            </span>
+                                                            <br />
+                                                            <div
+                                                                className="mt-2"
+                                                                style={{ fontSize: "10px", lineHeight: "12px" }}
+                                                            >
+                                                                Al Muhairi 113-127, Al Dhagaya
+                                                                <br />
+                                                                Dubai, United Arab Emirates<br></br>
+                                                                Phone: +97143375728<br></br>
+                                                                Email: support@triplover.ae
+                                                            </div>
+                                                        </address>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+
+                                        <table
+                                            class="table table-borderless table-sm"
+                                            style={{ fontSize: "10px", lineHeight: "8px" }}
+                                        >
+                                            <tbody>
+                                                <tr className="d-flex">
+                                                    <td className="bg-white" style={{ width: "70%" }}>
+                                                        <tr>
+                                                        </tr>
+                                                        <tr>
+                                                        </tr>
+                                                    </td>
+                                                    <td className="bg-white" style={{ width: "30%" }}>
+                                                        <tr>
+                                                            <td
+                                                                className="text-end fw-bold"
+
+                                                            >
+                                                                Invoice Number<span className="mx-2">:</span>
+                                                            </td>
+                                                            <td className="text-end" style={{ width: "7%" }}>
+                                                                {invoice?.invoiceNo}
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td
+                                                                className="text-end fw-bold"
+
+                                                            >
+                                                                Invoice Date<span className="mx-2">:</span>
+                                                            </td>
+                                                            <td className="text-end" style={{ width: "7%" }}>
+                                                                {/* {moment(ticketingList[0]?.issueDate).format("DD-MMMM-yyyy ddd")} */}
+                                                            </td>
+                                                        </tr>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+
+                                        <table className="table table-bordered table-sm" style={{ fontSize: "10px" }}>
+                                            <thead className="fw-bold text-dark">
+                                                <th>Details</th>
+                                                <th className="text-end">Price</th>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>{invoice?.purpose}</td>
+                                                    <td className="text-end">{invoice?.amount}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td className="fw-bold" colSpan={2} style={{ textAlign: "right" }}>
+                                                        Total:  AED {invoice?.amount}
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                        <div className="container pb-5 mt-2">
+                                            <div class="row text-start ms-1">
+                                                <b className="p-0" style={{ fontSize: "15px" }}>Terms & Conditions :</b>
+                                                <ul style={{ fontSize: "10px" }}>
+                                                    <li>
+                                                        This is a computer generated statement, hence does not
+                                                        require any signature
+                                                    </li>
+                                                    <li>
+                                                        {" "}
+                                                        Refunds & Cancellations are subject to Airline's
+                                                        approval.
+                                                    </li>
+                                                    <li>
+                                                        Kindly check all details carefully to avoid unnecessary
+                                                        complications.
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default InvoiceView;
---
+++ src/Pages/Queues/Expired.js
@@ -0,0 +1,239 @@
+import React, { useState } from 'react';
+import { Box, Button, Center, Spinner, Text } from "@chakra-ui/react";
+import Navbar from '../SharePages/Navbar/Navbar';
+import SideNavBar from '../SharePages/SideNavBar/SideNavBar';
+import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
+import { environment } from '../SharePages/Utility/environment';
+import axios from 'axios';
+import moment from "moment";
+import ReactPaginate from "react-paginate";
+
+const Expired = () => {
+    let [pageCount, setPageCount] = useState(0);
+    let [pageSize, setPageSize] = useState(10);
+    let [currentPageNumber, setCurrentPageNumber] = useState(1);
+    let [ticketingList, setTicketingList] = useState([]);
+    let [uniqueTransID, setUniqueTransID] = useState("");
+    let [fromDate, setFromDate] = useState("");
+    let [toDate, setToDate] = useState("");
+    let [gdsPnr, setGDSPNR] = useState("");
+
+    const [isTimeOut, setIsTimeOut] = useState(false);
+  
+    useEffect(() => {
+      setTimeout(() => setIsTimeOut(true), 10000);
+    }, []);
+
+    const handleGetList = (currentPage) => {
+        const getTicketingList = async () => {
+          let sendObj = {
+            agentId: sessionStorage.getItem("agentId") ?? 0,
+            uniqueTransID: uniqueTransID,
+            fromDate: fromDate,
+            toDate: toDate,
+            pnr: gdsPnr,
+            status: "Booking Cancelled",
+          };
+          const response = await axios.post(
+            environment.getTicketingList +
+              `?pageNumber=${currentPage}&pageSize=${pageSize}`,
+            sendObj,
+            environment.headerToken
+          );
+          //ticketingList= response.data;
+          setTicketingList(await response.data.data);
+          setPageCount(await response.data.totalPages);
+          console.log(ticketingList);
+        };
+        getTicketingList();
+      };
+    
+      const handlePageClick = async (data) => {
+        let currentPage = data.selected + 1;
+        setCurrentPageNumber(currentPage);
+      };
+
+      const handleBookedView = (utid) => {
+        window.open("/bookedview?utid=" + utid, "_blank");
+        //navigate("/ticket?utid="+utid,'_blank');
+      };
+
+    useEffect(() => {
+        handleGetList(currentPageNumber);
+      }, [currentPageNumber]);
+    return (
+        <div>
+        <Navbar></Navbar>
+         <SideNavBar></SideNavBar>
+         <ToastContainer />
+         <div className="content-wrapper search-panel-bg">
+      <section className="content-header"></section>
+      <section className="content">
+        <form
+          className="mx-5 mt-3"
+          encType="multipart/form-data"
+          style={{ minHeight: "500px" }}
+        >
+          <div className="card">
+            <div className="card-body">
+              <div className="m-4">
+                <div className="tab-content">
+                  <div className="tab-pane fade show active" id="tp1">
+                    <h4>Expired Booking</h4>
+                    <hr className="my-3" />
+                    <div
+                      style={{ overflowX: "scroll", marginBottom: "16px" }}
+                    >
+                      <table
+                        className="table table-bordered table-sm"
+                        style={{ width: "100%", fontSize: "13px" }}
+                      >
+                        <thead className="text-center fw-bold bg-secondary">
+                          <tr>
+                           <th>Booking Date</th>
+                            <th>Booking ID</th>
+                            <th className='text-start'>Passenger Name</th>
+                            <th className='text-start'>Passenger Type</th>
+                            {/* <th>Time Limit</th> */}
+                            <th>Flight Date</th>
+                            <th>Route</th>
+                            <th>PNR</th>
+                            {/* <th>Ticket Number</th> */}
+                            <th>Total Price</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody className="tbody">
+                        {ticketingList.length > 0 ? (
+                            ticketingList.map((item, index) => {
+                              return (
+                                <tr key={index}>
+                                   <td>
+                                    {moment(item.bookingDate).format(
+                                      "DD-MM-YYYY hh:mm:ss A"
+                                    )}
+                                  </td>
+                                  <td>
+                                    <a
+                                      href="javascript:void(0)"
+                                      onClick={() =>
+                                        handleBookedView(item.uniqueTransID)
+                                      }
+                                    >
+                                      {item.uniqueTransID}
+                                    </a>
+                                  </td>
+                                  <td className='text-start'>{item.leadPaxName}</td>
+                                  <td></td>
+                                  <td>
+                                    {item.departure != null ? (
+                                      moment(item.departure).format(
+                                        "DD-MM-YYYY hh:mm:ss A"
+                                      )
+                                    ) : (
+                                      <></>
+                                    )}
+                                  </td>
+                                  <td>
+                                  {
+                                    item.origin === "null"? "": 
+                                    item.journeyType=="Round Trip"?item.origin + (item.destination === "" ? "" : "⇔" + item.destination)
+                                    :item.origin + (item.destination === "" ? "" : "→" + item.destination)
+                                  }
+                                  </td>
+                                  
+                                  <td>
+                                    <a
+                                      href="javascript:void(0)"
+                                      onClick={() =>
+                                        handleBookedView(item.uniqueTransID)
+                                      }
+                                    >
+                                      {item.pnr}
+                                    </a>
+                                  </td>
+                                  <td>{item.ticketingPrice}</td>
+                                  <td>
+                                    {item.status} <br />{" "}
+                                  </td>
+                                  <td className="text-left">
+                                  
+                                      <>
+                                        <a href="javascript:void(0)"
+                                          title="View Booking"
+                                          onClick={() =>
+                                            handleBookedView(
+                                              item.uniqueTransID
+                                            )
+                                          }
+                                        >
+                                          <Button
+                                            border="2px solid"
+                                            colorScheme="messenger"
+                                            variant="outline"
+                                            size="xsm"
+                                            borderRadius="16px"
+                                            p="1"
+                                          >
+                                            <span style={{fontSize:"10px"}}>VB</span>
+                                          </Button>
+                                        </a>
+                                      </>
+                                    
+                                  </td>
+                                </tr>
+                              );
+                            })
+                          ) : (
+                            <></>
+                          )}
+                        </tbody>
+                      </table>
+
+                      {Object.keys(ticketingList).length === 0 && !isTimeOut && (
+                        <Center w="100%" py="50px">
+                          <Spinner
+                            thickness="4px"
+                            speed="0.65s"
+                            emptyColor="gray.200"
+                            color="red.500"
+                            size="xl"
+                          />
+                        </Center>
+                      )}
+                    </div>
+
+                    <ReactPaginate
+                      previousLabel={"previous"}
+                      nextLabel={"next"}
+                      breakLabel={"..."}
+                      pageCount={pageCount}
+                      marginPagesDisplayed={2}
+                      pageRangeDisplayed={3}
+                      onPageChange={handlePageClick}
+                      containerClassName={"pagination justify-content-center"}
+                      pageClassName={"page-item"}
+                      pageLinkClassName={"page-link"}
+                      previousClassName={"page-item"}
+                      previousLinkClassName={"page-link"}
+                      nextClassName={"page-item"}
+                      nextLinkClassName={"page-link"}
+                      breakClassName={"page-item"}
+                      breakLinkClassName={"page-link"}
+                      activeClassName={"active"}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </section>
+       </div>
+      </div>
+    );
+};
+
+export default Expired;

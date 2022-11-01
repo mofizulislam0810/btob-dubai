350fe472e01bb4564e669010b1b12358d07fbead
nur120 <nur.uddin2077@gmail.com>
2022-11-01 16:57:45 +0600

    ticket number required in support submit

--- src/common/functions.js
+++ src/common/functions.js
@@ -4,3 +4,10 @@ export const getCabinClass = (input) => {
   else if (input === "First") return 4;
   else return 1;
 };
+
+export const getPassengerType = (input) => {
+  if (input === "ADT") return "Adult";
+  else if (input === "CNN") return "Child";
+  else if (input === "INF") return "Infant";
+  else return "Adult";
+};
